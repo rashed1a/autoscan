ae8770b8ae0b5321f71dd9be9557d004fd901f83
Md Rashed Hossain <50524317+rashed1a@users.noreply.github.com>
2024-07-08 16:37:21 +0600

    Update update_os.py

--- update_os.py
+++ update_os.py
@@ -1,4 +1,21 @@
-print("For update your Linux OS you can use this command")
-print("sudo apt-get update")
-print("For upgrade your Linux OS you can use this command")
-print("sudo apt-get upgrade")
+import subprocess
+import os
+
+print("")
+print("Device Information...")
+device_data=subprocess.run(['hostnamectl'], capture_output=True, text=True, check=True)
+print(device_data.stdout)
+
+print("Updating your device...")
+update1 = ['sudo', 'apt-get', 'update']
+try:
+    update2 = subprocess.run(update1, capture_output=True, text=True, check=True)
+    print(update2.stdout)
+
+except subprocess.CalledProcessError as e:
+    print(f"Error occurred: {e}")
+    print(f"Error output: {e.stderr}")
+
+print("Update Complete...")
+cwd = os.getcwd()
+print(f"Current directory: {cwd}")
