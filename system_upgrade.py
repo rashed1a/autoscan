3113a79be99d107f68381137162f9fbda8df6dad
Md Rashed Hossain <50524317+rashed1a@users.noreply.github.com>
2024-07-08 16:55:53 +0600

    Create system_upgrade.py

---
+++ system_upgrade.py
@@ -0,0 +1,36 @@
+import subprocess
+import os
+
+print("")
+print("Device Information...")    
+device_data=subprocess.run(['hostnamectl'], capture_output=True, text=True, check=True)
+print(device_data.stdout)
+
+print("Updating your device...")  
+update1 = ['sudo', 'apt-get', 'update']
+try:
+    update2 = subprocess.run(update1, capture_output=True, text=True, check=True)
+    print(update2.stdout)
+
+except subprocess.CalledProcessError as e:
+    print(f"Error occurred: {e}") 
+    print(f"Error output: {e.stderr}")
+
+print("Update Complete...")
+cwd = os.getcwd()
+print(f"Current directory: {cwd}")
+
+print("")
+print("Upgrading your device...")
+update1 = ['sudo', 'apt-get', 'upgrade']
+try:
+    update2 = subprocess.run(update1, capture_output=True, text=True, check=True)
+    print(update2.stdout)
+
+except subprocess.CalledProcessError as e:
+    print(f"Error occurred: {e}")
+    print(f"Error output: {e.stderr}")
+
+print("Upgrade Complete...")
+cwd = os.getcwd()
+print(f"Current directory: {cwd}")
